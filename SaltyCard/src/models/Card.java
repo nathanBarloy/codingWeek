04e653074d4819a05ebd168dfc686d98b0bd4a06
olivier Gouellain <olivier.gouellain@telecomnancy.eu>
2018-12-17 09:31:18 +0100

    stack

--- SaltyCard/src/models/Card.java
+++ SaltyCard/src/models/Card.java
@@ -1,4 +1,20 @@
 package models;
 
 public class Card {
+    private  String name;
+    private  String text;
+    private  String answer;
+
+    Card (String name, String text, String answer){
+        this.name=name;
+        this.texte=texte;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTexte() {
+        return texte;
+    }
 }
