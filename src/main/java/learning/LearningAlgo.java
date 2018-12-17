fb4b3cc75bae2dd54c1592424528f17945a4a05e
olivier Gouellain <olivier.gouellain@telecomnancy.eu>
2018-12-17 16:15:32 +0100

    algo

--- src/main/java/learning/LearningAlgo.java
+++ src/main/java/learning/LearningAlgo.java
@@ -1,7 +1,26 @@
 package learning;
 
+import models.Card;
+import models.CardStack;
+
+import java.util.LinkedList;
+
+
 public class LearningAlgo {
-    int waitingtime = 10 ;
+    /*gère la selection des cartes à afficher*/
+    private int waitingtime = 10 ;
+    private CardStack cardstack;
+    private LinkedList<Card> cardlistshow;
+
+    LearningAlgo (CardStack cardstack ){
+        this.cardstack = cardstack;
+    }
+
+    public Card getCard(){
+        cardlistshow  = new LinkedList<Card>();
+        return cardstack.pop();
+
+    }
 
 
 }
---
+++ src/main/java/statistic/Stat.java
@@ -0,0 +1,5 @@
+package statistic;
+
+public class Stat {
+    /*récupère les stats utilisateurs*/
+}
