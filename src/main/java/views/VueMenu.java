7896b78a9d5dd72a4949fef4926579a89be4efff
BARLOY Nathan <nathan.barloy@telecomnancy.eu>
2018-12-17 15:06:05 +0100

    le menu il est la

--- src/main/java/views/VueMenu.java
+++ src/main/java/views/VueMenu.java
@@ -1,4 +1,11 @@
 package views;
 
+import launch.Main;
+
 public class VueMenu {
+
+    public void lancerModeTest() {
+        System.out.println("salut");
+        Main.main.switchScene("../views/VueQuestion.fxml");
+    }
 }
