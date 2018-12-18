1c7e5277dd47372620b6856e3c0e65dd7b17a3de
olivier Gouellain <olivier.gouellain@telecomnancy.eu>
2018-12-18 12:14:24 +0100

    Database update

--- src/main/java/database/Database.java
+++ src/main/java/database/Database.java
@@ -1,21 +1,37 @@
 package database;
 
+import models.CardStack;
+
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Database {
     private List<String> listdecks;
+    private List<CardStack>  listCardStack;
 
 
     public Database() {
     listdecks=new ArrayList<String>();
     }
 
-    public List<String> getListdecks() {
+    public List<String> getListStack() {
         return listdecks;
     }
 
-    public void setListdecks(List<String> listdecks) {
+    public void setListStack(List<String> liststack) {
         this.listdecks = listdecks;
+    }
+
+    public List<CardStack> getListCardStack() {
+        return listCardStack;
+    }
+
+    public void setListCardStack(List<CardStack> listCardSatck) {
+        this.listCardStack = listCardStack;
+    }
+
+    public void addCardStack(CardStack CardStack) {
+        this.listCardStack.add(CardStack);
     }
 }
