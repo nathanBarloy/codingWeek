c004f804ff3e0e91ddb80897252aa22bc4640db3
Alexis EL MRINI <alexis.el-mrini@telecomnancy.eu>
2018-12-18 17:03:51 +0100

    erreur corrigée

--- src/main/java/seeds/PackageGenerator.java
+++ src/main/java/seeds/PackageGenerator.java
@@ -2,14 +2,17 @@ package seeds;
 
 import models.Card;
 import models.CardStack;
+import models.Player;
 
 public class PackageGenerator {
-    Card card1 = new Card("la vérité blesse", "Qui est le plus salé du groupe? ", "Alexis<3");
-    Card card2 = new Card("sql", "Qui est le génie du sql? ", "Alexis");
-    Card card3 = new Card("temps", "Quelle heure est-il? ", "L'heure d'une petite pause");
-    CardStack cardstack1 = new CardStack("test1", "pour test ");
+    private Player player;
+    private CardStack cardStack;
 
     public PackageGenerator() {
+
+        player=new Player("Olivier", "azerty");
+        cardStack=new CardStack("test", "test", player);
+        CardStackSeed cardStackSeed = new CardStackSeed(cardStack,player);
     }
 /*
     public CardStackPackageGenerator(int nb , int seed){
