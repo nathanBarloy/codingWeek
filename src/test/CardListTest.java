61c8760ffe1f495ee97387c6163c5a2a89066296
olivier Gouellain <olivier.gouellain@telecomnancy.eu>
2018-12-19 10:12:54 +0100

    Cardlist test ok

---
+++ src/test/CardListTest.java
@@ -0,0 +1,43 @@
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+
+import database.Database;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import models.*;
+import seeds.CardStackSeed;
+
+
+public class CardListTest {
+
+    private CardList cardList;
+
+    @Before
+    public void initialiser() throws Exception {
+        this.cardList = new CardList();
+        CardStackSeed cardStackSeed = new CardStackSeed(this.cardList);
+        cardStackSeed.seed();
+        System.out.println(this.cardList.getSize());
+    }
+
+    @After
+    public void nettoyer() throws Exception {
+    }
+
+    @Test
+    public void card() {
+
+        assertNotNull("card pas créée", cardList);
+        assertEquals("carte1 ", "Carte 1", cardList.getCard().getName());
+        assertEquals("carte2 ", "Carte 2" , cardList.getnextCard().getName());
+        assertEquals("carte3", "Carte 3", cardList.getnextCard().getName());
+        assertEquals("fin ", false, cardList.endCardList());
+
+
+    }
+
+}
