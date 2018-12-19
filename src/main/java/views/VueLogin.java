421369429958816c8c8b246e106cbbb27b0d74ff
Aurélien Delage <aurelien.delage@telecomnancy.eu>
2018-12-19 20:09:29 +0100

    lolllilol

--- src/main/java/views/VueLogin.java
+++ src/main/java/views/VueLogin.java
@@ -3,6 +3,9 @@ package views;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
 import launch.Main;
 import models.Partie;
 import models.Player;
@@ -17,6 +20,9 @@ import java.util.Observer;
 public class VueLogin implements Observer {
 
     private Partie partie;
+
+    @FXML
+    private Pane pane;
 
     @FXML
     private TextField utilisateur;
@@ -60,6 +66,17 @@ public class VueLogin implements Observer {
 
     @Override
     public void update(Observable o, Object arg) {
+        Image image0 = new Image("/resources/img/flashcards.png");
 
+        //final URL imageURL = getClass().getResource("../ressources/fond");
+        //final Image image1 = new Image(imageURL.toExternalForm());
+
+        ImageView img = new ImageView();
+        img.setImage(image0);
+        img.setFitHeight(275);
+        img.setFitWidth(700);
+        img.setLayoutX(200);
+
+        this.pane.getChildren().add(img);
     }
 }
