990b8de56c68500112d902bc70139889b48ea1f9
Alexis EL MRINI <alexis.el-mrini@telecomnancy.eu>
2018-12-19 14:51:19 +0100

    add ping query

---
+++ src/main/java/queries/QueryPing.java
@@ -0,0 +1,39 @@
+package queries;
+
+import models.Card;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.IOException;
+
+public class QueryPing extends Query {
+
+
+    public QueryPing() {
+        super("ping");
+        
+    }
+
+
+    public void send() throws IOException {
+
+
+// Request parameters and other properties.
+
+//Execute and get the response.
+            HttpResponse httpResponse = httpClient.execute(request);
+            HttpEntity entity = httpResponse.getEntity();
+
+            if (entity != null) {
+                try  {
+                    this.response = entity.getContent();
+                }catch (Exception e){
+
+                }
+            }
+
+
+    }
+}
