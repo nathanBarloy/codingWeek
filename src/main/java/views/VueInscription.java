c4a0368d9c8d2c1dbd125c05b8a11b75246c2451
Alexis EL MRINI <alexis.el-mrini@telecomnancy.eu>
2018-12-20 13:50:11 +0100

    add debug to login

--- src/main/java/views/VueInscription.java
+++ src/main/java/views/VueInscription.java
@@ -50,13 +50,13 @@ public class VueInscription implements Observer{
         if ( !(nom.length()<3 || mdp.length()<6 || !mdp.equals(confirm)) ) { //si informations sont correctes
             //ajouter utilisateur à la BDD
             Query check = new QueryCheckUsername(nom);
-            String resp = "1";
+            String resp = "error";
 
                 check.send();
                 resp = check.getResponse(); //on regarde si le nom entré existe déjà
                 System.out.println(resp);
 
-            if (resp.equals("0")) { //si le nom n'existe pas (cas correct)
+            if (resp.equals("1")) { //si le nom n'existe pas (cas correct)
                 Player player = new Player(nom);
 
                 Query query = new QueryAddUser(nom,mdp);
